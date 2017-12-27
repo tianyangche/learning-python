895829b2b08fc0cc72e301cde9dea0184b479f58
tianyangche <tianyangche@gmail.com>
2017-12-26 23:01:28 -0800

    Adding Practicing User Inputs

---
+++ src/practice_user_inputs.py
@@ -0,0 +1,9 @@
+# message = input('Tell me something, and I will repeat it back to you: ')
+# print(message)
+
+while True:
+        s = raw_input('Enter something : ')
+        if s == 'quit':
+                break
+        print('Length of the string is', len(s))
+print('Done')
